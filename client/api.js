e146040120cf7132bc664c6083e8d34d50cac280
RMH <riki@craftsmoothie.co.nz>
2019-02-21 12:00:03 +1300

    Added API function

--- client/api.js
+++ client/api.js
@@ -2,5 +2,11 @@ import request from 'superagent'
 
 const userUrl '#'
 
-export function getUsers (callback)
-  
+export function getUsers (callback) {
+  request
+    .get(userUrl)
+    .end((err, res) => {
+      callback(err, res.body)
+    })
+}
+
