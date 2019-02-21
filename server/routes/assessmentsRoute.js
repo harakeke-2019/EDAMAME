a564031c75ce5e4c5af6ca00f1ed607f1cb52dd3
rebecca-zhang <rebeccazhangnz@outlook.com>
2019-02-21 15:06:49 +1300

    fixed linter error

--- server/routes/assessmentsRoute.js
+++ server/routes/assessmentsRoute.js
@@ -4,15 +4,14 @@ const assessmentsRouter = express.Router()
 
 router.get('/assessments/:id', (req, res) => {
   const id = req.params.id
-  
 })
 
-
-router.put ('/assessments/:id',(req,res)=>{
+router.put('/assessments/:id', (req, res) => {
   const assID = req.params.id
   const evidenceInfo = req.body
-  db.updateEvidence(assID,evidenceInfo)
-    .catch(err =>res.status(500).json({error: 'Fail to add evidence to assesment!'}))
+  db.updateEvidence(assID, evidenceInfo).catch(err =>
+    res.status(500).json({ error: 'Fail to add evidence to assesment!' })
+  )
 })
 
 module.exports = assessmentsRouter
