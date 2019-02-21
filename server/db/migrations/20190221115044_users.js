d78257ae6d88d2aaf36a13f8edd10ed410e27eca
Jessica-Morgan <jess-morgan1@outlook.com>
2019-02-21 15:50:27 +1300

    Added surname to users table

--- server/db/migrations/20190221115044_users.js
+++ server/db/migrations/20190221115044_users.js
@@ -2,6 +2,7 @@ exports.up = function (knex, Promise) {
   return knex.schema.createTable('users', table => {
     table.increments('id').primary()
     table.string('name')
+    table.string('surname')
     table.string('hash')
     table.string('role')
   })
