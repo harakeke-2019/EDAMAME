56676d0ed602d509d742b0db0c92f0c0e22c1f88
Jenny Fang <yanfang196888@gmail.com>
2019-02-21 14:34:22 +1300

    add assessmentsRoute

--- server/assessmentsDb.js
+++ server/assessmentsDb.js
@@ -3,7 +3,10 @@ const config = require('./knexfile')[environment]
 const connection = require('knex')(config)
 
 function getAssessments(id, db=connection){
-    
+    return db('Assessments')
+    .join('Students_Assessments', 'id', 'assessment_id')
+    .where('id',id)
+    .select('title', 'description', 'evidence')
 }
 module.exports ={
     getAssessments,
--- server/routes/assessmentsRoute.js
+++ server/routes/assessmentsRoute.js
@@ -1,10 +1,14 @@
 const express = require('express')
-
 const assessmentsRouter = express.Router()
+const assessmentsDb = require('./assessmentsDb')
 
 router.get('/assessments/:id', (req, res) => {
   const id = req.params.id
-  
+  assessmentsDb.getAssessments(id)
+    .then(assessment=>res.json(assessment))
+    .catch(err => {
+        res.status(500).send(err.message)
+      })
 })
 
 
