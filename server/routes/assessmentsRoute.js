78cc61f8ff7b4c2634b9c1e17c1e6277998be6b4
rebecca-zhang <rebeccazhangnz@outlook.com>
2019-02-21 13:38:41 +1300

    Added assessment/:id put route

--- server/routes/assessmentsRoute.js
+++ server/routes/assessmentsRoute.js
@@ -8,4 +8,12 @@ router.get('/assessments/:id', (req, res) => {
 })
 
 
+router.put ('/assessments/:id',(req,res)=>{
+  const assessmentID = req.params.id
+  const evidence = req.body.evidence
+
+  db.updateEvidence(assessmentID,evidence)
+    .catch(err =>res.status(500).json({error: 'Fail to add evidence to assesment!'}))
+})
+
 module.exports = assessmentsRouter
