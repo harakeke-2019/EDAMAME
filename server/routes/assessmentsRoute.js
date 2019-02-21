a765aff596bcae76e833fc809affe722bed15bc7
Jenny Fang <yanfang196888@gmail.com>
2019-02-21 15:29:33 +1300

    update assessmentsRoute

--- server/routes/assessmentsRoute.js
+++ server/routes/assessmentsRoute.js
@@ -2,7 +2,7 @@ const express = require('express')
 const assessmentsRouter = express.Router()
 const assessmentsDb = require('./assessmentsDb')
 
-router.get('/assessments/:id', (req, res) => {
+router.get('/:id', (req, res) => {
   const id = req.params.id
   assessmentsDb.getAssessments(id)
     .then(assessment=>res.json(assessment))
--- server/server.js
+++ server/server.js
@@ -4,10 +4,11 @@ const express = require('express')
 const server = express()
 
 const starterRoute = require('./routes/starter')
+const assessmentsRoute = require ('./routes/assessmentsRoute')
 
 server.use(express.json())
 server.use(express.static(path.join(__dirname, './public')))
 
 server.use('/', starterRoute)
-
+server.use('/api/v1/assessments', assessmentsRoute)
 module.exports = server
