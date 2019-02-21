a25f3d0acfe830c16b1038175813ad2ddd786c69
aubreytekanawa <aubreytekanawa@gmail.com>
2019-02-21 16:23:14 +1300

    DropdownModule file

---
+++ client/components/DropdownModule.jsx
@@ -0,0 +1,50 @@
+// Aubrey Ngapeita David
+import React, { Component } from 'react';
+
+class DropdownModule extends Component {
+render () {
+    return (
+    <div>
+        <h1>[placeholder - Assessment Completion Component]</h1>
+    <div>
+        <h2>student dashboard</h2>
+        <p>[Metric placeholder 1]   [Metric placeholder 2]    [Metric placeholder 3]</p>
+
+    </div>
+
+    <ol>
+        <div>
+            <h2>[Module One]</h2>
+                <li>Assessment </li>
+                <li>Assessment </li>
+                <li>Assessment </li>
+                <li>Assessment </li>      
+        </div>
+        <div>
+            <h2>[Module Two]</h2>
+                <li>Assessment </li>
+                <li>Assessment </li>
+                <li>Assessment </li>
+                <li>Assessment </li>      
+        </div>
+        <div>
+            <h2>[Module Three]</h2>
+                <li>Assessment </li>
+                <li>Assessment </li>
+                <li>Assessment </li>
+                <li>Assessment </li>      
+        </div>
+        <div>
+            <h2>[Module Four]</h2>
+                <li>Assessment </li>
+                <li>Assessment </li>
+                <li>Assessment </li>
+                <li>Assessment </li>      
+        </div>
+    </ol>
+    </div>
+    )
+}
+}
+
+export default DropdownModule
