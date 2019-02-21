8faf253d2bd2d621fa0042ea012bc1514ca3f7a7
Lane le Prevost-Smith <lanelps@gmail.com>
2019-02-21 16:25:53 +1300

    header complete

---
+++ client/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div className="header">
+      <h1>Name</h1>
+      <h1>Surname</h1>
+      <h3>Cohort</h3>
+    </div>
+  )
+}
