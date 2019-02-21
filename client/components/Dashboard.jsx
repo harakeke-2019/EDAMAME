e014657c500bab0afde2fff560ccd05d8423fb4b
Lane le Prevost-Smith <lanelps@gmail.com>
2019-02-21 16:50:43 +1300

    made changes

--- client/components/Dashboard.jsx
+++ client/components/Dashboard.jsx
@@ -1,28 +1,10 @@
 import React from 'react'
 
-import Header from './Header'
-import ProgressBar from './ProgressBar'
-import ProgressModule from './ProgressModule'
-import DropdownModule from './DropdownModule'
 
 export default function Dashboard() {
   return (
     <React.Fragment>
-      <div>
-        <Header />
-      </div>
 
-      <div>
-        <ProgressBar />
-      </div>
-
-      <div>
-        <ProgressModule />
-      </div>
-
-      <div>
-        <DropdownModule />
-      </div>
     </React.Fragment>
   )
 }
