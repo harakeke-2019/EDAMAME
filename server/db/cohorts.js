c51f9e15eafd1027a1f0f2ee526035184fac639a
Jessica-Morgan <jess-morgan1@outlook.com>
2019-02-21 13:26:17 +1300

    added get route for all cohorts

---
+++ server/db/cohorts.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const db = require('../db')
+const router = express.Router()
+
+module.exports = {
+    getAllCohorts
+}
+
+function getAllCohorts () {
+    return db('cohorts')
+    .select('name', 'year', 'campus')
+}
+
