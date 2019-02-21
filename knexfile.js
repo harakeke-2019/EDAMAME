a6d119fcb5dc689fd14c59e89a1314f5ab9726ee
Jason James Simpson <jasonjamessimpson@gmail.com>
2019-02-21 15:49:19 +1300

    Ammendment to fix sqlite error

--- knexfile.js
+++ knexfile.js
@@ -1,3 +1,5 @@
+// Update with your config settings.
+
 module.exports = {
 
   development: {
@@ -14,17 +16,29 @@ module.exports = {
     }
   },
 
-  test: {
-    client: 'sqlite3',
+  staging: {
+    client: 'postgresql',
     connection: {
-      filename: ':memory:'
+      database: 'my_db',
+      user: 'username',
+      password: 'password'
     },
-    useNullAsDefault: true
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
   },
 
   production: {
     client: 'postgresql',
-    connection: process.env.DATABASE_URL,
+    connection: {
+      database: 'my_db',
+      user: 'username',
+      password: 'password'
+    },
     pool: {
       min: 2,
       max: 10
