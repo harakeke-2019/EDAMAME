f69b496856ec7cbf736a1caf50c4b585bca2da0c
RMH <riki@craftsmoothie.co.nz>
2019-02-21 14:28:30 +1300

    added api url and db function to get users

--- client/api.js
+++ client/api.js
@@ -1,12 +1,12 @@
 import request from 'superagent'
 
-const userUrl '#'
+const userUrl 'http://localhost:3000/api/v1'
 
 export function getUsers (callback) {
   request
-    .get(userUrl)
-    .end((err, res) => {
-      callback(err, res.body)
+    .get(`${userUrl}/users`)
+    .then(res => res.body)
+    .catch(err => {
+      if (err) throw Error('Cannot get users')
     })
 }
-
