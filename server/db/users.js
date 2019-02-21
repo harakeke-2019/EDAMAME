be9badd43cb2b96d8106f05062ce01f7316ffcd2
Fetu-o-le-moana Teuila Tamapeau <fetu.tamapeau@gmail.com>
2019-02-21 19:15:59 +1300

    Save progress

--- server/db/users.js
+++ server/db/users.js
@@ -3,20 +3,24 @@ const config = require('./knexfile')[environment]
 const connection = require('knex')(config)
 
 module.exports = {
-  newUser: newUser,
-  addCohort: addCohort
+  newUser: newUser
 }
 
 // newUser db function for post register route
-function newUser (name, surname, role, password, db = connection) {
+function newUser (user, db = connection) {
   return db('users')
     .returning('id')
-    .insert({name, surname, role, hash: password})
+    .insert({
+      name: user.name,
+      surname: user.surname,
+      role: user.role,
+      hash: user.password
+    })
 }
 
 // addCohort db function
 
-function addCohort (cohort, db = connection) {
-  return db('students')
-    .insert({cohort})
-}
+// function addCohort (id, cohort, db = connection) {
+//   return db('students')
+//     .insert({cohort})
+// }
--- server/routes/usersRoutes.js
+++ server/routes/usersRoutes.js
@@ -1,21 +1,16 @@
 const express = require('express')
 
-const db = require('../db')
+const db = require('../db/users')
 
 const router = express.Router()
 
 // route to register new user (auth routes prefixed with /api/v1/auth)
 
 router.post('/register', (req, res) => {
-  const name = req.body.name
-  const surname = req.body.surname
-  const role = req.body.role
-  const cohort = req.body.cohort
-  // const email = req.body.email
-  const password = req.body.hash // Do we need an extra param for password confirmation? Also what encryption stuff do we need here?
+  db.newUser(req.body)
 
-  db.newUser(name, surname, role, password)
-    .then(id => db.addCohort(id[0], cohort))
+  // .then(id => db.addCohort(id[0], cohort))
+
     .then(res.redirect('/')) // redirect to relevant path for a registered user.
 
     .catch(displayErr)
