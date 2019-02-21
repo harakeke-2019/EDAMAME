a2e29fce6207f83b6351ad786cbd4ed4c9ac40d6
rebecca-zhang <rebeccazhangnz@outlook.com>
2019-02-21 15:07:20 +1300

    fixed linter error

--- server/assessmentsDb.js
+++ server/assessmentsDb.js
@@ -2,20 +2,18 @@ const environment = process.env.NODE_ENV || 'development'
 const config = require('./knexfile')[environment]
 const connection = require('knex')(config)
 
-function getAssessments(id, db=connection){
-    
-}
+function getAssessments(id, db = connection) {}
 
-function updateEvidence(assId, evidenceInfo, db){
+function updateEvidence(assId, evidenceInfo, db) {
   return db('Students_Assessments')
-        .where('assessment_id',assId)
-        .update({evidence: evidenceInfo.evidence,
-                 date_modified: evidenceInfo.date })
+    .where('assessment_id', assId)
+    .update({
+      evidence: evidenceInfo.evidence,
+      date_modified: evidenceInfo.date
+    })
 }
 
-
-
-module.exports ={
-    getAssessments,
-    updateEvidence
+module.exports = {
+  getAssessments,
+  updateEvidence
 }
