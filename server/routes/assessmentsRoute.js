10f861938c7d9de74fb981f943205807aa31de2c
rebecca-zhang <rebeccazhangnz@outlook.com>
2019-02-21 17:42:18 +1300

    fixed file path error

--- server/routes/assessmentsRoute.js
+++ server/routes/assessmentsRoute.js
@@ -1,17 +1,16 @@
 const express = require('express')
-
-const assessmentsRouter = express.Router()
+const router = express.Router()
+const db = require('../assessmentsDb')
 
 router.get('/:id', (req, res) => {
   const id = req.params.id
 })
 
 router.put('/:id', (req, res) => {
-  const id = req.params.id
   const evidence = req.body
   db.updateEvidence(id, evidence).catch(err =>
     res.status(500).json({ error: 'Fail to add evidence to assessment!' })
   )
 })
 
-module.exports = assessmentsRouter
+module.exports = router
