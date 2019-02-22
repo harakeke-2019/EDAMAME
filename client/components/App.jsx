09d034456462f7b01cdce9e10e6bfde064e2f657
Jess Morgan <jess-morgan1@outlook.com>
2019-02-22 00:57:02 +0000

    Added route to registration form

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
-import { Route } from 'react-router-dom'
+import {Route} from 'react-router-dom'
 
 import Login from './Login'
+import RegistrationForm from './ResgistrationForm'
 
 const App = () => {
   return (
     <React.Fragment>
       <Route Login path="/" component={Login} />
+      <Route path="/register" component={RegistrationForm}/>
     </React.Fragment>
   )
 }
