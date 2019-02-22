da9146b760a852793aea3c761fa2b3e419ddb220
Lane le Prevost-Smith <lanelps@gmail.com>
2019-02-22 13:45:58 +1300

    Added ProgressModule Components to Dashboard, with place holder props

--- client/components/Dashboard.jsx
+++ client/components/Dashboard.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 
+import ProgressModule from './ProgressModule'
 
 export default function Dashboard() {
+  const placeHolderProps = {
+    tally: 20
+  }
   return (
     <React.Fragment>
+      <div className="progress-module">
+        <ProgressModule placeholder={placeHolderProps} />
+        <div>Module 1</div>
 
+        <ProgressModule placeholder={placeHolderProps} />
+        <div>Module 2</div>
+
+        <ProgressModule placeholder={placeHolderProps} />
+        <div>Module 3</div>
+
+        <ProgressModule placeholder={placeHolderProps} />
+        <div>Module 4</div>
+      </div>
     </React.Fragment>
   )
 }
