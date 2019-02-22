1f7d209e1dbb0eb70c24f1ba05a62de8d6e7048f
RMH <riki@craftsmoothie.co.nz>
2019-02-22 14:03:32 +1300

    Ammendmants made as per request

--- server/server.js
+++ server/server.js
@@ -4,14 +4,14 @@ const express = require('express')
 const server = express()
 
 const starterRoute = require('./routes/starter')
-const users = require('./routes/usersRoutes')
+const usersRoutes = require('./routes/users')
 const cohortRoutes = require('./routes/cohortRoutes')
 
 server.use(express.json())
 server.use(express.static(path.join(__dirname, './public')))
 
 server.use('/', starterRoute)
-server.use('/api/v1/users', users)
+server.use('/api/v1/users', usersRoutes)
 server.use('/api/v1/cohorts', cohortRoutes)
 
 module.exports = server
