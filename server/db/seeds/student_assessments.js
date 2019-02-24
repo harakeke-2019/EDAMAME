1fadc6df2a3e3507865123983b3ce189dc17ac9a
rebecca-zhang <rebeccazhangnz@outlook.com>
2019-02-25 10:58:08 +1300

    added seed for put route test

--- server/db/seeds/student_assessments.js
+++ server/db/seeds/student_assessments.js
@@ -1,33 +1,58 @@
-exports.seed = function (knex, Promise) {
+exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('student_assessments').del()
-    .then(function () {
+  return knex('student_assessments')
+    .del()
+    .then(function() {
       // Inserts seed entries
       return knex('student_assessments').insert([
-        {id: 1,
+        {
+          id: 1,
           student_id: 1,
           assessment_id: 1,
           status_id: 4,
           evidence: 'evidence',
-          date_modified: 'date'},
-        {id: 2,
+          date_modified: 'date'
+        },
+        {
+          id: 2,
           student_id: 1,
           assessment_id: 2,
           status_id: 3,
           evidence: 'evidence',
-          date_modified: 'date'},
-        {id: 3,
+          date_modified: 'date'
+        },
+        {
+          id: 3,
           student_id: 1,
           assessment_id: 3,
           status_id: 2,
           evidence: 'evidence',
-          date_modified: 'date'},
-        {id: 4,
+          date_modified: 'date'
+        },
+        {
+          id: 4,
           student_id: 1,
           assessment_id: 4,
           status_id: 1,
           evidence: 'evidence',
-          date_modified: 'date'}
+          date_modified: 'date'
+        },
+        {
+          id: 5,
+          student_id: 2,
+          assessment_id: 3,
+          status_id: 2,
+          evidence: 'evidence',
+          date_modified: 'date'
+        },
+        {
+          id: 6,
+          student_id: 3,
+          assessment_id: 2,
+          status_id: 1,
+          evidence: 'evidence',
+          date_modified: 'date'
+        }
       ])
     })
 }
