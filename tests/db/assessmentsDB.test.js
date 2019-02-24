6ea39d196c487879de933dcb2d2e3fc7dc87b208
rebecca-zhang <rebeccazhangnz@outlook.com>
2019-02-25 12:42:41 +1300

    defined testDb

--- tests/db/assessmentsDB.test.js
+++ tests/db/assessmentsDB.test.js
@@ -1,5 +1,7 @@
 const testEnv = require('./test-environment')
-const db = require('../../server/db/assessmentsDb')
+const db = require('../../server/db/assessments')
+
+let testDb = null
 
 beforeEach(() => {
   testDb = testEnv.getTestDb()
@@ -11,7 +13,8 @@ afterEach(() => testEnv.cleanup(testDb))
 test('updateEvidence updates evidence and date in student_assignment db', () => {
   const newEvidence = {
     evidence: 'new evidence3',
-    date: '2019-01-23 09:30:05'
+    date: '2019-01-23 09:30:05',
+    studentId : 2
   }
   const id = 3
 
