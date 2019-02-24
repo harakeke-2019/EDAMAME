1cbc25b45ace59d08ede327b4f27e93751fa10ed
Chris Laupama <chrislaupama@icloud.com>
2019-02-25 12:20:36 +1300

    renamed to use the correct naming conventions for EDAMAME

--- server/server.js
+++ server/server.js
@@ -3,16 +3,15 @@ const express = require('express')
 
 const server = express()
 
-const starterRoute = require('./routes/starter')
-const usersRoutes = require('./routes/usersRoutes')
-const cohortRoutes = require('./routes/cohortRoutes')
-const assessmentsRoutes = require('./routes/assessmentsRoutes')
+const assessmentsRoutes = require('./routes/assessments')
+const cohortsRoutes = require('./routes/cohorts')
+const usersRoutes = require('./routes/users')
 
 server.use(express.json())
 server.use(express.static(path.join(__dirname, './public')))
 
-server.use('/', starterRoute)
 server.use('/api/v1/assessments', assessmentsRoutes)
+server.use('/api/v1/cohorts', cohortsRoutes)
 server.use('/api/v1/users', usersRoutes)
-server.use('/api/v1/cohorts', cohortRoutes)
+
 module.exports = server
