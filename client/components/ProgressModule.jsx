0c1eebf6d7590dcf2c1ba5b95a6324b00014b4d1
Lane le Prevost-Smith <lanelps@gmail.com>
2019-02-25 11:45:28 +1300

    'props.placeHolderProps.percentage' to 'props.placeholder.percentage'

--- client/components/ProgressModule.jsx
+++ client/components/ProgressModule.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import CircularProgressbar from 'react-circular-progressbar'
 
 export default function ProgressModule(props) {
-  const percentage = props.placeHolderProps.percentage
+  const percentage = props.placeholder.percentage
   return (
     <React.Fragment>
       <CircularProgressbar percentage={percentage} text={`${percentage}%`} />
