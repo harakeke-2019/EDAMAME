79bc11964c937e56b27aef91af1ce96f00f556ce
Benjamin Peter Little <benlittle53@gmail.com>
2019-02-25 10:43:19 +1300

    changes made to fetch assesment id and also changed file name

---
+++ client/api/assessments.js
@@ -0,0 +1,12 @@
+import request from 'superagent'
+
+const URL = 'http:localhost:3000/v1/api'
+
+export const assessmentContent = (assesmentId) => {
+  return request
+    .get(`${URL}/assessments/${assesmentId}`)
+    .then(res => res.body)
+    .catch(err => {
+      throw new Error(err)
+    })
+}
