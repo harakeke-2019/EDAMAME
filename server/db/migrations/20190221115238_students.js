086d0b849ce658c55ab62164ab60deb0d320c561
Ariane Thompson <ari.r.thompson@gmail.com>
2019-02-26 09:59:04 +1300

    updated relational integrity across db tables or something

--- server/db/migrations/20190221115238_students.js
+++ server/db/migrations/20190221115238_students.js
@@ -1,8 +1,8 @@
 exports.up = function (knex, Promise) {
   return knex.schema.createTable('students', table => {
     table.increments('id').primary()
-    table.integer('user_id')
-    table.integer('cohort_id')
+    table.integer('user_id').references('users.id')
+    table.integer('cohort_id').references('cohorts.id')
     table.string('name')
   })
 }
