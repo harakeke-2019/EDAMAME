4178da5f78c5bbe7544787dc9b5ef26931ccbd2d
Benjamin Peter Little <benlittle53@gmail.com>
2019-02-26 08:15:27 +1300

    added thunk middleware

--- client/index.js
+++ client/index.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {createStore} from 'redux'
+import {applyMiddleware, compose, createStore} from 'redux'
 import {BrowserRouter as Router} from 'react-router-dom'
 import {Provider} from 'react-redux'
+import thunkMiddleware from 'redux-thunk'
 
 import App from './components/App'
 import reducer from './reducer'
 
-// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
-const store = createStore(reducer)
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const store = createStore(
+  reducer,
+  composeEnhancers(applyMiddleware(thunkMiddleware))
+)
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
