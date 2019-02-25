da8ebb2804e38574d57305d21372b74db796187e
rebecca-zhang <rebeccazhangnz@outlook.com>
2019-02-25 14:56:23 +1300

    updated test to test error

--- tests/db/assessmentsDB.test.js
+++ tests/db/assessmentsDB.test.js
@@ -14,11 +14,12 @@ test('updateEvidence updates evidence and date in student_assignment db', () => 
   const newEvidence = {
     evidence: 'new evidence3',
     date: '2019-01-23 09:30:05',
-    studentId : 2
+    studentId : 1
   }
   const id = 3
 
   return db
     .updateEvidence(id, newEvidence, testDb)
     .then(updated => expect(updated).toBe(1))
+    .catch(err => expect(err).toBeNull())
 })
