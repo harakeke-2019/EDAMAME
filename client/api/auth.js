45b835f873ef15e4086222affc90d21de70e886f
Haixin Zhang <rebeccazhangnz@outlook.com>
2019-02-26 11:46:28 +1300

    change url for auth api only

--- client/api/auth.js
+++ client/api/auth.js
@@ -1,10 +1,10 @@
 import request from 'superagent'
 
-const url = 'http://localhost:3000'
+const url = 'http://localhost:3000/api/v1/auth'
 
 export const signin = (user) => {
   return request
-    .post(`${url}/api/v1/auth/signin`)
+    .post(`${url}/signin`)
     .send({ user })
     .then(res => res.body)
 }
