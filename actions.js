b9e9de4e99e1103d8afb207aeec9ece72baf247d
ngapeita <ngapeita@.com>
2019-02-25 15:39:25 +1300

    actions file

---
+++ actions.js
@@ -0,0 +1,28 @@
+import {createStore} from 'redux' // creates a store
+
+// The untouched state
+const initialAssessmentState = {
+
+}
+
+// Reducer and actions
+// Handles an array of objects which are filled with assessment info
+const AssessmentContent = (state = initialAssessmentState, action) => {
+  switch (action.type) {
+    case 'REQUEST_STUDENT_ASSESSMENT_STATUSES':
+      return {
+        assessment: ?
+      }
+    case 'RECIEVE_STUDENT_ASSESSMENT_STATUSES':
+      return {
+        assessment: ?
+      }
+      default:
+      return state
+  }
+}
+
+// Enables us to view our store in the redux devtools
+const store = createStore(AssessmentContent, 
+  window.__REDUX_DEVTOOLS_EXTENSION__&&window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
