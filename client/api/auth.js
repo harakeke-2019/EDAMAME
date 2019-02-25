7c585970b5e8950203d838237871a369e5108c55
Francine Burling-Claridge <francinebc.mail@gmail.com>
2019-02-26 10:23:09 +1300

    Wrote register api

---
+++ client/api/auth.js
@@ -0,0 +1,10 @@
+import request from 'superagent'
+
+const url = 'http://localhost:3000'
+
+export function register (user) {
+  return request
+    .get(`${url}/api/v1/auth/register`)
+    .send(user)
+    .then(res => res.body)
+}
