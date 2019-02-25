8e8a5ab78d4ae3acffe71d6ffd8172a39abef8e7
Jessica Morgan <jess-morgan1@outlook.com>
2019-02-26 10:39:06 +1300

    drop down showing all assessment titles

--- client/components/dropdownModule/DropdownModule.jsx
+++ client/components/dropdownModule/DropdownModule.jsx
@@ -31,7 +31,8 @@ const styles = theme => ({
 class DropdownModule extends Component {
   state = {
     expanded: null,
-    module1: {
+    module1: { 
+      assessment1: {
       moduleId: 1,
       moduleTitle: 'Module 1: Core Programming',
       assessmentsId: 1,
@@ -40,48 +41,305 @@ class DropdownModule extends Component {
       description: 'desc',
       link: 'link',
       week_day: 'weekday',
-      exerciseId: 1,
+      exerciseId: null,
       exerciseTitle:
-        'https://github.com/dev-academy-challenges/taxonomic-routing'
+        ''
+    },
+    assessment2: {
+        moduleId: 1,
+        moduleTitle: 'Module 1: Core Programming',
+        assessmentsId: 2,
+        assessmentsTitle:
+          '2. Use npm to manage library dependencies',
+        description: 'desc',
+        link: 'link',
+        week_day: 'weekday',
+        exerciseId: null,
+        exerciseTitle:
+          ''
+    },
+    assessment3: {
+      moduleId: 1,
+      moduleTitle: 'Module 1: Core Programming',
+      assessmentsId: 3,
+      assessmentsTitle:
+        '3. Create well structured and tested code using JavaScript ES6',
+      description: 'desc',
+      link: 'link',
+      week_day: 'weekday',
+      exerciseId: null,
+      exerciseTitle:
+        ''
+    },
+    assessment4: {
+      moduleId: 1,
+      moduleTitle: 'Module 1: Core Programming',
+      assessmentsId: 4,
+      assessmentsTitle:
+        '4. Design, build and use a relational database to persist data',
+      description: 'desc',
+      link: 'link',
+      week_day: 'weekday',
+      exerciseId: null,
+      exerciseTitle:
+        ''
+    },
+    assessment5: {
+      moduleId: 1,
+      moduleTitle: 'Module 1: Core Programming',
+      assessmentsId: 5,
+      assessmentsTitle:
+        '5. Create code which reads and writes to the filesystem',
+      description: 'desc',
+      link: 'link',
+      week_day: 'weekday',
+      exerciseId: null,
+      exerciseTitle:
+        ''
+    },
+    assessment6: {
+      moduleId: 1,
+      moduleTitle: 'Module 1: Core Programming',
+      assessmentsId: 6,
+      assessmentsTitle:
+        '6. Build a Command Line Interface tool',
+      description: 'desc',
+      link: 'link',
+      week_day: 'weekday',
+      exerciseId: null,
+      exerciseTitle:
+        ''
+    },
     },
     module2: {
+      assessment1: {
       moduleId: 2,
       moduleTitle: 'Module 2: Web development',
-      assessmentsId: 2,
+      assessmentsId: 7,
+      assessmentsTitle:
+        '1. Produce and deploy a tested HTTP server with a restful JSON API',
+      description: 'desc',
+      link: 'link',
+      week_day: 'weekday',
+      exerciseId: null,
+      exerciseTitle:
+        ''
+    },
+    assessment2:{
+      moduleId: 2,
+      moduleTitle: 'Module 2: Web development',
+      assessmentsId: 8,
       assessmentsTitle:
         '2. Create both server and client code that consumes a HTTP API',
       description: 'desc',
       link: 'link',
       week_day: 'weekday',
-      exerciseId: 2,
+      exerciseId: null,
       exerciseTitle:
-        'https://github.com/dev-academy-challenges/knex-relationships-stories'
+        ''
     },
-    module3: {
-      moduleId: 3,
-      moduleTitle: 'Module 3: Team Work and Leadership',
-      assessmentsId: 3,
+    assessment3: {
+      moduleId: 2,
+      moduleTitle: 'Module 2: Web development',
+      assessmentsId: 9,
       assessmentsTitle:
-        '3. Manage a programming team using agile management techniques',
+        '3. Create a rich client application using React and Redux',
       description: 'desc',
       link: 'link',
       week_day: 'weekday',
-      exerciseId: 3,
-      exerciseTitle: 'Group projects'
+      exerciseId: null,
+      exerciseTitle:
+        ''
+    },
+    assessment4: {
+      moduleId: 2,
+      moduleTitle: 'Module 2: Web development',
+      assessmentsId: 10,
+      assessmentsTitle:
+        '4. Create a responsive website using HTML and CSS',
+      description: 'desc',
+      link: 'link',
+      week_day: 'weekday',
+      exerciseId: null,
+      exerciseTitle:
+        ''
+    },
+    assessment5: {
+      moduleId: 2,
+      moduleTitle: 'Module 2: Web development',
+      assessmentsId: 11,
+      assessmentsTitle:
+        '5. Use routing libraries to manage large client and server code bases ',
+      description: 'desc',
+      link: 'link',
+      week_day: 'weekday',
+      exerciseId: null,
+      exerciseTitle:
+        ''
+    },
+    assessment6: {
+      moduleId: 2,
+      moduleTitle: 'Module 2: Web development',
+      assessmentsId: 12,
+      assessmentsTitle:
+        '6. Secure a web application using Authentication libraries',
+      description: 'desc',
+      link: 'link',
+      week_day: 'weekday',
+      exerciseId: null,
+      exerciseTitle:
+        ''
+    },
+  },
+    module3: {
+      assessment1: {
+        moduleId: 3,
+      moduleTitle: 'Module 3: Team Work and Leadership',
+      assessmentsId: 13,
+      assessmentsTitle:
+        '1. Manage a programming team using agile management techniques',
+      description: 'desc',
+      link: 'link',
+      week_day: 'weekday',
+      exerciseId: null,
+      exerciseTitle: ''
+      },
+      assessment2: {
+        moduleId: 3,
+        moduleTitle: 'Module 3: Team Work and Leadership',
+        assessmentsId: 14,
+        assessmentsTitle:
+          '2. Give and receive constructive feedback to/from team mates',
+        description: 'desc',
+        link: 'link',
+        week_day: 'weekday',
+        exerciseId: null,
+        exerciseTitle: ''
+      },
+      assessment3: {
+        moduleId: 3,
+        moduleTitle: 'Module 3: Team Work and Leadership',
+        assessmentsId: 15,
+        assessmentsTitle:
+          '3. Present a technical project to a non-technical audience',
+        description: 'desc',
+        link: 'link',
+        week_day: 'weekday',
+        exerciseId: null,
+        exerciseTitle: ''
+      },
+      assessment4: {
+        moduleId: 3,
+        moduleTitle: 'Module 3: Team Work and Leadership',
+        assessmentsId: 16,
+        assessmentsTitle:
+          '4. Research a novel technology and teach it to others',
+        description: 'desc',
+        link: 'link',
+        week_day: 'weekday',
+        exerciseId: null,
+        exerciseTitle: ''
+      },
+      assessment5: {
+        moduleId: 3,
+        moduleTitle: 'Module 3: Team Work and Leadership',
+        assessmentsId: 17,
+        assessmentsTitle:
+          '5. Deliver a code review and provide technical feedback',
+        description: 'desc',
+        link: 'link',
+        week_day: 'weekday',
+        exerciseId: null,
+        exerciseTitle: ''
+      },
+      assessment6: {
+        moduleId: 3,
+        moduleTitle: 'Module 3: Team Work and Leadership',
+        assessmentsId: 18,
+        assessmentsTitle:
+          '6. Review a presentation and provide meaningful feedback',
+        description: 'desc',
+        link: 'link',
+        week_day: 'weekday',
+        exerciseId: null,
+        exerciseTitle: ''
+      },
     },
     module4: {
+      assessment1: {
       moduleId: 4,
       moduleTitle: 'Module 4: Human Skills',
-      assessmentsId: 4,
+      assessmentsId: 19,
+      assessmentsTitle:
+        '1. Create and follow a self-directed learning and personal development plan',
+      description: 'desc',
+      link: 'link',
+      week_day: 'weekday',
+      exerciseId: null,
+      exerciseTitle: ''
+    },
+    assessment2: {
+      moduleId: 4,
+      moduleTitle: 'Module 4: Human Skills',
+      assessmentsId: 20,
       assessmentsTitle:
         '2. Practice attentive listening and intentional clarifying questions',
       description: 'desc',
       link: 'link',
       week_day: 'weekday',
-      exerciseId: 4,
-      exerciseTitle: 'Observation'
+      exerciseId: null,
+      exerciseTitle: ''
+    },
+    assessment3: {
+      moduleId: 4,
+      moduleTitle: 'Module 4: Human Skills',
+      assessmentsId: 21,
+      assessmentsTitle:
+        '3. Display inclusive and supportive behaviour towards all team mates',
+      description: 'desc',
+      link: 'link',
+      week_day: 'weekday',
+      exerciseId: null,
+      exerciseTitle: ''
+    },
+    assessment4: {
+      moduleId: 4,
+      moduleTitle: 'Module 4: Human Skills',
+      assessmentsId: 22,
+      assessmentsTitle:
+        '4. Resolve interpersonal conflict in a technical project',
+      description: 'desc',
+      link: 'link',
+      week_day: 'weekday',
+      exerciseId: null,
+      exerciseTitle: ''
+    },
+    assessment5: {
+      moduleId: 4,
+      moduleTitle: 'Module 4: Human Skills',
+      assessmentsId: 23,
+      assessmentsTitle:
+        '5. Hold a difficult conversation',
+      description: 'desc',
+      link: 'link',
+      week_day: 'weekday',
+      exerciseId: null,
+      exerciseTitle: ''
+    },
+    assessment6: {
+      moduleId: 4,
+      moduleTitle: 'Module 4: Human Skills',
+      assessmentsId: 24,
+      assessmentsTitle:
+        '6. Actively contribute to creating a productive and inclusive team culture',
+      description: 'desc',
+      link: 'link',
+      week_day: 'weekday',
+      exerciseId: null,
+      exerciseTitle: ''
     }
   }
+}
 
   handleChange = panel => (event, expanded) => {
     this.setState({
@@ -102,15 +360,37 @@ class DropdownModule extends Component {
           >
             <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
               <Typography className={classes.heading}>
-                {this.state.module1.moduleTitle}
+                {this.state.module1.assessment1.moduleTitle}
               </Typography>
             </ExpansionPanelSummary>
-            <ExpansionPanelDetails className={classes.description}>
+            <ExpansionPanelDetails>
               <Typography>
-                {this.state.module1.assessmentsTitle}
-                <br />
-                <span>Exercise: </span>
-                <a href="#">{this.state.module1.exerciseTitle}</a>
+                <a href="#">{this.state.module1.assessment1.assessmentsTitle}</a>
+              </Typography>
+            </ExpansionPanelDetails>
+            <ExpansionPanelDetails>
+              <Typography>
+              <a href="#">{this.state.module1.assessment2.assessmentsTitle}</a>
+              </Typography>
+            </ExpansionPanelDetails>
+            <ExpansionPanelDetails>
+              <Typography>
+              <a href="#">{this.state.module1.assessment3.assessmentsTitle}</a>
+              </Typography>
+            </ExpansionPanelDetails>
+            <ExpansionPanelDetails>
+              <Typography>
+              <a href="#">{this.state.module1.assessment4.assessmentsTitle}</a>
+              </Typography>
+            </ExpansionPanelDetails>
+            <ExpansionPanelDetails>
+              <Typography>
+              <a href="#">{this.state.module1.assessment5.assessmentsTitle}</a>
+              </Typography>
+            </ExpansionPanelDetails>
+            <ExpansionPanelDetails>
+              <Typography>
+              <a href="#">{this.state.module1.assessment6.assessmentsTitle}</a>
               </Typography>
             </ExpansionPanelDetails>
           </ExpansionPanel>
@@ -121,15 +401,37 @@ class DropdownModule extends Component {
           >
             <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
               <Typography className={classes.heading}>
-                {this.state.module2.moduleTitle}
+                {this.state.module2.assessment1.moduleTitle}
               </Typography>
             </ExpansionPanelSummary>
             <ExpansionPanelDetails>
               <Typography>
-                {this.state.module2.assessmentsTitle}
-                <br />
-                <span>Exercise: </span>
-                <a href="#">{this.state.module2.exerciseTitle}</a>
+                <a href="#">{this.state.module2.assessment1.assessmentsTitle}</a>
+              </Typography>
+            </ExpansionPanelDetails>
+            <ExpansionPanelDetails>
+              <Typography>
+              <a href="#">{this.state.module2.assessment2.assessmentsTitle}</a>
+              </Typography>
+            </ExpansionPanelDetails>
+            <ExpansionPanelDetails>
+              <Typography>
+              <a href="#">{this.state.module2.assessment3.assessmentsTitle}</a>
+              </Typography>
+            </ExpansionPanelDetails>
+            <ExpansionPanelDetails>
+              <Typography>
+              <a href="#">{this.state.module2.assessment4.assessmentsTitle}</a>
+              </Typography>
+            </ExpansionPanelDetails>
+            <ExpansionPanelDetails>
+              <Typography>
+              <a href="#">{this.state.module2.assessment5.assessmentsTitle}</a>
+              </Typography>
+            </ExpansionPanelDetails>
+            <ExpansionPanelDetails>
+              <Typography>
+              <a href="#">{this.state.module2.assessment6.assessmentsTitle}</a>
               </Typography>
             </ExpansionPanelDetails>
           </ExpansionPanel>
@@ -140,15 +442,37 @@ class DropdownModule extends Component {
           >
             <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
               <Typography className={classes.heading}>
-                {this.state.module3.moduleTitle}
+                {this.state.module3.assessment1.moduleTitle}
               </Typography>
             </ExpansionPanelSummary>
             <ExpansionPanelDetails>
               <Typography>
-                {this.state.module3.assessmentsTitle}
-                <br />
-                <span>Exercise: </span>
-                <a href="#">{this.state.module3.exerciseTitle}</a>
+                <a href="#">{this.state.module3.assessment1.assessmentsTitle}</a>
+              </Typography>
+            </ExpansionPanelDetails>
+            <ExpansionPanelDetails>
+              <Typography>
+              <a href="#">{this.state.module3.assessment2.assessmentsTitle}</a>
+              </Typography>
+            </ExpansionPanelDetails>
+            <ExpansionPanelDetails>
+              <Typography>
+              <a href="#">{this.state.module3.assessment3.assessmentsTitle}</a>
+              </Typography>
+            </ExpansionPanelDetails>
+            <ExpansionPanelDetails>
+              <Typography>
+              <a href="#">{this.state.module3.assessment4.assessmentsTitle}</a>
+              </Typography>
+            </ExpansionPanelDetails>
+            <ExpansionPanelDetails>
+              <Typography>
+              <a href="#">{this.state.module3.assessment5.assessmentsTitle}</a>
+              </Typography>
+            </ExpansionPanelDetails>
+            <ExpansionPanelDetails>
+              <Typography>
+              <a href="#">{this.state.module3.assessment6.assessmentsTitle}</a>
               </Typography>
             </ExpansionPanelDetails>
           </ExpansionPanel>
@@ -159,15 +483,37 @@ class DropdownModule extends Component {
           >
             <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
               <Typography className={classes.heading}>
-                {this.state.module4.moduleTitle}
+                {this.state.module4.assessment1.moduleTitle}
               </Typography>
             </ExpansionPanelSummary>
             <ExpansionPanelDetails>
               <Typography>
-                {this.state.module4.assessmentsTitle}
-                <br />
-                <span>Exercise: </span>
-                <a href="#">{this.state.module4.exerciseTitle}</a>
+                <a href="#">{this.state.module4.assessment1.assessmentsTitle}</a>
+              </Typography>
+            </ExpansionPanelDetails>
+            <ExpansionPanelDetails>
+              <Typography>
+              <a href="#">{this.state.module4.assessment2.assessmentsTitle}</a>
+              </Typography>
+            </ExpansionPanelDetails>
+            <ExpansionPanelDetails>
+              <Typography>
+              <a href="#">{this.state.module4.assessment3.assessmentsTitle}</a>
+              </Typography>
+            </ExpansionPanelDetails>
+            <ExpansionPanelDetails>
+              <Typography>
+              <a href="#">{this.state.module4.assessment4.assessmentsTitle}</a>
+              </Typography>
+            </ExpansionPanelDetails>
+            <ExpansionPanelDetails>
+              <Typography>
+              <a href="#">{this.state.module4.assessment5.assessmentsTitle}</a>
+              </Typography>
+            </ExpansionPanelDetails>
+            <ExpansionPanelDetails>
+              <Typography>
+              <a href="#">{this.state.module4.assessment6.assessmentsTitle}</a>
               </Typography>
             </ExpansionPanelDetails>
           </ExpansionPanel>
