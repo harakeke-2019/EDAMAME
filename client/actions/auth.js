e76cf6b0405b0b0d354030ffd214fcbca3ce6b0e
Benjamin Peter Little <benlittle53@gmail.com>
2019-02-26 08:10:04 +1300

    fixed typos

--- client/actions/auth.js
+++ client/actions/auth.js
@@ -20,15 +20,16 @@ export const registerError = error => {
   }
 }
 
-export const register = (name, surname, password) => {
-  request
+export const register = (name, surname, password) => dispatch => {
+  dispatch(registerPending())
+  return request
     .get('http:localhost:3000/api/v1/auth/register')
     .send({name, surname, password})
     .then(res => {
       setToken(res.body.token)
       dispatch(registerSuccess())
     })
-    .catch(err => registerError(err.response.body.error))
+    .catch(err => dispatch(registerError(err.response.body.error)))
 }
 
 export const logout = _ => {
--- client/reducer/auth.js
+++ client/reducer/auth.js
@@ -1,43 +1,42 @@
 import {getToken} from '../utils/tokens'
 
 const initialState = {
-    error: null,
-    loggedIn: !!getToken(), //Coerces object to boolean, so either returns true or false
-    newRegistration: false,
-    pending: false
+  error: null,
+  loggedIn: !!getToken(), // Coerces object to boolean, so either returns true or false
+  newRegistration: false,
+  pending: false
 }
 
-export default authReducer = (state = initialState, action) => {
-    switch(action.type){
-        case 'REGISTER_PENDING':
-            return {
-                ...state,
-                loggedIn: false,
-                pending: true
-            }
-        case 'REGISTER_SUCCESS':
-            return {
-                ...state,
-                error: null,
-                newRegistration: true,
-                //able to access token
-                loggedIn: true,
-                pending: false
-            }
-        case 'REGISTER_ERROR':  
-            return {
-                ...state,
-                error,
-                loggedIn: false,
-                pending: false
+export default function authReducer (state = initialState, action) {
+  switch (action.type) {
+    case 'REGISTER_PENDING':
+      return {
+        ...state,
+        loggedIn: false,
+        pending: true
+      }
+    case 'REGISTER_SUCCESS':
+      return {
+        ...state,
+        error: null,
+        newRegistration: true,
+        loggedIn: true,
+        pending: false
+      }
+    case 'REGISTER_ERROR':
+      return {
+        ...state,
+        error: true,
+        loggedIn: false,
+        pending: false
 
-            }
-        case 'LOGOUT':
-            return {
-                ...state,
-                loggedIn: false
-            }
-        default:    
-            return state
-    }
+      }
+    case 'LOGOUT':
+      return {
+        ...state,
+        loggedIn: false
+      }
+    default:
+      return state
+  }
 }
