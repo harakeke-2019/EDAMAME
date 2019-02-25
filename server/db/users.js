f69dbd662f2ce5b3b09373377c64f7e95adcbe69
Jason James Simpson <jasonjamessimpson@gmail.com>
2019-02-25 15:10:00 +1300

    Function specifics ammended

--- server/db/users.js
+++ server/db/users.js
@@ -24,10 +24,20 @@ function newUser (user, db = connection) {
 
 function getStudentAssessmentStatuses (id, db = connection) {
   return db('student_assessments')
+    .join('exercises', 'student_assessments.assessment_id', 'exercises.assessment_id')
+    .join('statuses', 'student_assessments.status_id', 'statuses.id')
     .join('assessments', 'student_assessments.assessment_id', 'assessments.id')
     .join('modules', 'assessments.module_id', 'modules.id')
     .where('student_assessments.student_id', id)
-    .select()
+    .select(
+      'student_assessments.evidence as evidence',
+      'exercises.title as excersiseTitle',
+      'statuses.name as status',
+      'assessments.title as assessmentTitle',
+      'student_assessments.student_id as studentId',
+      'modules.title as moduleTitle',
+      'assessments.description as assessmentDescription'
+    )
 }
 
 // addCohort db function
