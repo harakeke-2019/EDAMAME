6f35b886f27daa8018ba9d03cac511cf680a2b51
ngapeita <ngapeita@.com>
2019-02-26 09:56:30 +1300

    delete pseudocode

--- client/index.js
+++ client/index.js
@@ -3,10 +3,8 @@ import ReactDOM from 'react-dom'
 import {Provider} from 'react-redux'
 import {createStore} from 'redux'
 
-// Imports our app
 import App from './components/App'
 
-// Enables us to view our store in the redux devtools
 const store = createStore(AssessmentContent,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
