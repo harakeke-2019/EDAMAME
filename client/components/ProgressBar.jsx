82dc17e29d28385d4ab28f6114e4dead4d9f7583
Yan Fang <yanfang196888@gmail.com>
2019-02-26 15:51:32 +1300

    progress bar now showing

--- client/components/ProgressBar.jsx
+++ client/components/ProgressBar.jsx
@@ -5,16 +5,14 @@ export default function ProgressBar (props) {
   const percentage1 = 10
   const percentage2 = 20
   const percentage3 = 30
-  const percentage4 = 15
 
   return (
     <React.Fragment>
-      <h3>Cohort Progress</h3>
-      <Bar className='progressBar'>
-        <Bar striped variant="success" now={{percentage1}} key={1} />
-        <Bar variant="warning" now={{percentage2}} key={2} />
-        <Bar striped variant="danger" now={{percentage3}} key={3} />
-        <Bar striped variant="info" now={{percentage4}} key={4} />
+      <h4>Cohort Progress</h4>
+      <Bar>
+        <Bar striped variant="success" now={percentage1} key={1} />
+        <Bar variant="warning" now={percentage2} key={2} />
+        <Bar striped variant="danger" now={percentage3} key={3} />
       </Bar>
     </React.Fragment>
   )
