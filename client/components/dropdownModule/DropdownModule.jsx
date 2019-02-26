9f7d23d8af1b33401fa47ba5aa4f102ff645e992
Jenny Fang <yanfang196888@gmail.com>
2019-02-26 14:43:26 +1300

    working on assessment actions

--- client/components/dropdownModule/DropdownModule.jsx
+++ client/components/dropdownModule/DropdownModule.jsx
@@ -1,31 +1,12 @@
 // Aubrey Ngapeita David
 import React from 'react'
 import Modules from './Modules'
-// import {getModules} from '../../api'
 
 const DropdownModule = () => {
-//    constructor (props){
-//        super(props)
-//        this.state={
-//             moduleList:[]
-//        }
-//    }
 
-  //    componentDidMount(){
-  //        getModules()
-  //         .then(moduleList=>{
-  //             this.setState({moduleList})
-  //         })
-  //         .catch(err=>console.log('error',err))
-  //    }
   return (
     <React.Fragment>
       <h1>DropdownModule Component</h1>
-      <ul>
-        {/* {this.state.moduleList.map(module => {
-          return (<li>module</li>
-        })} */}
-      </ul>
       <Modules />
     </React.Fragment>
   )
