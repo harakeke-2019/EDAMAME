1a811a5d05140522727cc4a9c288b9f6c0d66ab4
Grantis <grantrigby1992@gmail.com>
2019-02-26 15:52:50 +1300

    browserRouter

--- client/components/SignIn.jsx
+++ client/components/SignIn.jsx
@@ -2,22 +2,30 @@ import React from 'react'
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button'
 
-export default function SignIn() {
-  return (
-    <div>
-      <h1>SIGN IN COMPONENT</h1>
-      <TextField
-        varient="outlined"
-        label="firstname" />
-      <TextField
-        varient="outlined"
-        label="lastname" />
-      <TextField
-        varient="outlined"
-        label="password" />
-      <Button variant="contained" color="primary">
-        Login
+class SignIn extends React.Component() {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div>
+        <TextField
+          varient="outlined"
+          label="firstname"
+        />
+        <TextField
+          varient="outlined"
+          label="lastname" />
+        <TextField
+          varient="outlined"
+          label="password" />
+        <Button variant="contained" color="primary" >
+          Login
       </Button>
-    </div>
-  )
+      </div>
+    )
+  }
 }
+
+export default SignIn 
