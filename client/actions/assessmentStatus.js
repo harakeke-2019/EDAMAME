6951f410312bd333b9cb75d4e2c66213da674da2
Ngapeita Matehaere Leilua-Gotz <ngapeita@outlook.com>
2019-02-26 15:42:24 +1300

    renamed actions file

--- client/actions/assessmentStatus.js
+++ client/actions/assessmentStatus.js
@@ -1,17 +1,28 @@
-export default {
-  requestAssessmentStatus,
-  receiveAssessmentStatus
-}
+import request from 'superagent'
 
-function requestAssessmentStatus () {
+const url = 'http://localhost:3000/api/v1/users/student'
+
+// ACTIONS
+export function requestAssessmentStatus () {
   return {
     type: 'REQUEST_STUDENT_ASSESSMENT_STATUS'
   }
 }
 
-function receiveAssessmentStatus (assessmentStatus) {
+export function receiveAssessmentStatus (assessmentStatus) {
   return {
     type: 'RECEIVE_STUDENT_ASSESSMENT_STATUS',
     assessmentStatus: assessmentStatus
+  }
+}
+
+export function getAssessmentStatus (id) {
+  return (dispatch) => {
+    dispatch(requestAssessmentStatus())
+    request.get(`${url}/${id}?view=modules`)
+      .then(res => {
+        dispatch(receiveAssessmentStatus(res.body))
+      })
+      .catch(err => console.log(err))
   }
 }
