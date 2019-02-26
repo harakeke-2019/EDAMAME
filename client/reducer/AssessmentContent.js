e41b276e7e40358b76e3520f0d75545edb2f4615
Micah Nathaniel Titus Peautagi <micahpeautagi@gmail.com>
2019-02-26 13:47:20 +1300

    initial state specified

--- client/reducer/AssessmentContent.js
+++ client/reducer/AssessmentContent.js
@@ -1,19 +1,30 @@
 import {createStore} from 'redux'
 
+// Similar to Beers exercise, I am writing the initial state that changes depending on whatever is below.
 
-// REDUCER 
+// In initialState we set our properties to nothing. Because depending on the actions in our reducer, it will fill in these properties with different things.
+
+const initialState = {
+  evidence: '',
+  excersiseTitle: '',
+  status: '',
+  assessmentTitle: '',
+  studentId: '',
+  moduleTitle: '',
+  assessmentDescription: ''
+}
+// REDUCER
 // This reducer handles an array of objects filled with assessment info
 
-// Rewrite the state within the parenthesis
-const AssessmentContent = (state = initialWombatState, action) => {
+const AssessmentStatuses = (state = initialState, action) => {
   switch (action.type) {
     case 'REQUEST_STUDENT_ASSESSMENT_STATUS':
       return {
-   
+
       }
     case 'RECEIVE_STUDENT_ASSESSMENT_STATUS':
       return {
-       
+
       }
     default:
       return state
